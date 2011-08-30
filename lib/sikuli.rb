bb00da330e43f76adc792d4b2f03108f9953b70e
Konstantin Shabanov <etehtsea@gmail.com>
2011-08-30 22:13:11 +0400

    initial linux support

--- lib/sikuli.rb
+++ lib/sikuli.rb
@@ -1,4 +1,17 @@
-require "sikuli/sikuli-script.jar"
+require 'rbconfig'
+require 'java'
+
+case RbConfig::CONFIG['target_os']
+when "darwin"
+  require 'sikuli/osx/sikuli-script.jar'
+when "linux"
+  case RbConfig::CONFIG['target_cpu']
+  when "i386"   ; require 'sikuli/linux/sikuli-script-x86.jar'
+  when "x86_64" ; require 'sikuli/linux/sikuli-script-x86_64.jar'
+  else raise "Platform not supported yet"
+  end
+end
+
 require "sikuli/version"
 
 require "sikuli/app"
